ed43c98fb503ead7b6ce13c4f975c4899e7ad2de
DiaPaus <dia_doc@yahoo.com>
2024-06-06 11:53:23 +0300

    options are now logged capitalized as requested

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
 const getComputerChoice=function(){
-const choices=['rock','paper','scissors'];
+const choices=['Rock','Paper','Scissors'];
 return choices[Math.floor(Math.random()*3)]
 }
 
 const getHumanChoice=function(){
     let humanChoice='';
     while(!['rock','paper','scissors'].includes(humanChoice)) humanChoice=(prompt('Your choice? rock, papers or scissors?').toLowerCase());
-    return humanChoice;
+    return humanChoice[0].toUpperCase()+humanChoice.substring(1)
 }
-
 
 const playGame=function(){
     let humanScore=0, computerScore=0;
     const playRound=function(computerChoice,humanChoice){
-
-        let winnerCases={
-            'paper':'rock',
-            'scissors':'paper',
-            'rock':'scissors'
+    let winnerCases={
+            'Paper':'Rock',
+            'Scissors':'Paper',
+            'Rock':'Scissors'
         }
-    
-        if (winnerCases[computerChoice]===humanChoice) 
-            {console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    if(computerChoice===humanChoice) console.log(`It's a tie! ${(computerChoice)} = ${(humanChoice)}`)
+    else if (winnerCases[computerChoice]===humanChoice) 
+            {console.log(`You lose! ${(computerChoice)} beats ${(humanChoice)}`);
             computerScore++;}
-            else 
-            {console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    else 
+            {console.log(`You win! ${(humanChoice)} beats ${(computerChoice)}`);
             humanScore++;}
     }
 
@@ -35,7 +33,8 @@ const playGame=function(){
     }
 
     if(humanScore>computerScore) console.log(`You win! ${humanScore} to ${computerScore}`)
-        else console.log(`You loose! ${computerScore} to ${humanScore}`)
+    else if (humanScore<computerScore) console.log(`You loose! ${computerScore} to ${humanScore}`)
+    else console.log(`It's a tie ${computerScore} to ${humanScore}`)
 }
 
 playGame()
