611703f4cf523ecdb7dae8e03dd8043eee5c7448
DiaPaus <dia_doc@yahoo.com>
2024-06-07 12:52:25 +0300

    All working

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 const choices=['Rock','Paper','Scissors'];
 let humanScore=0, computerScore=0;
+let playing=true;
 
 const getComputerChoice=function(){
 return choices[Math.floor(Math.random()*3)]
@@ -14,20 +15,35 @@ const playRound=function(computerChoice,humanChoice){
             'Scissors':'Paper',
             'Rock':'Scissors'
         }
-    if(computerChoice===humanChoice) console.log(`It's a tie! ${(computerChoice)} = ${(humanChoice)}`)
-    else if (winnerCases[computerChoice]===humanChoice) 
-            {console.log(`You lose! ${(computerChoice)} beats ${(humanChoice)}`);
-            computerScore++;}
-    else 
-            {console.log(`You win! ${(humanChoice)} beats ${(computerChoice)}`);
+    if(playing&&computerChoice===humanChoice) results.innerText=`It's a tie! ${(computerChoice)} = ${(humanChoice)}`
+    else if (playing&&winnerCases[computerChoice]===humanChoice) 
+            {   computerScore++;
+                results.innerText=`You lose! ${(computerChoice)} beats ${(humanChoice)}!`
+                
+           }
+    else if(playing)
+            {results.innerText=`You win! ${(humanChoice)} beats ${(computerChoice)}`;
             humanScore++;}
+    if(humanScore<5&&computerScore<5)
+    {
+        results.innerText=(`${results.innerText}
+            You: ${humanScore} Computer: ${computerScore}`)
     }
-     
+    else if (computerScore===5) { results.innerText=(`Computer wins!!!!!
 
-    // if(humanScore>computerScore) console.log(`You win! ${humanScore} to ${computerScore}`)
-    // else if (humanScore<computerScore) console.log(`You loose! ${computerScore} to ${humanScore}`)
-    // else console.log(`It's a tie ${computerScore} to ${humanScore}`)
-// }
+
+        Computer: ${computerScore} You: ${humanScore}`);
+        playing=false;
+        setTimeout(()=>document.location.reload(),2000);
+    }
+    
+    else { results.innerText=(`You win!!!!!
+
+
+        You: ${humanScore} Computer: ${computerScore}`);
+        playing=false;
+        setTimeout(()=>document.location.reload(),2000);}           
+    }   
 
 const body=document.querySelector('body');
 
@@ -36,6 +52,20 @@ for(let choice of choices){
     element.id=`${choice.toLowerCase()}`;
     element.value=`${choice}`
     element.innerHTML=`${choice.toUpperCase()}`;
+    element.style.padding='30px';
+    element.style.background='#C292A1';
+    element.style.color='white';
     element.addEventListener('click',playRound);
     body.appendChild(element);
 }
+
+const results=document.createElement('div');
+results.style.background='lightgreen';
+results.style.color='#874C62';
+results.style.height='200px';
+results.style.width='350px';
+results.style.display='flex';
+results.style.justifyContent='center';
+results.style.alignItems='center';
+
+body.appendChild(results);
